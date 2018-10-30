46b96b7e692af59dc36520f8437e47c92a595d87
BT_1N3_30 <josrio3438@bths.edu>
2018-10-30 10:15:18 -0400

    Initial commit

---
+++ src/Room/Board.java
@@ -0,0 +1,7 @@
+package Board;
+
+public class Room {
+    Room[][] Maze = new Room[8][8];
+
+    for(int i = 0; i < Room.length ; i++)
+}
---
+++ src/Runner.java
@@ -0,0 +1,11 @@
+import People.Player;
+import Room.Board;
+
+public class Runner {
+    public static void main(String[] args)
+    {
+        // setup player, Difficulty and introduce
+        Player player1 = new Player();
+        Board[0][0].enterRoom;
+    }
+}
